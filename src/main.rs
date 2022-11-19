69e9dc4b70f11836af85c66a96737e047652489e
Alessio Giacobbe <alessiogiacobbe@MacBook-Pro-di-Alessio.local>
2022-11-19 01:41:58 +0100

    Created Op codes parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod op_codes_parser;
+
+use std::{fs};
+use std::collections::HashMap;
+use serde_json::{Value};
+use crate::op_codes_parser::op_codes_parser::Instruction;
+use crate::op_codes_parser::op_codes_parser::get_instructions_from_json;
+
 fn main() {
-    println!("Hello, world!");
+    let op_codes_content = fs::read_to_string("./src/opcodes.json").expect("LogRocket: error reading file");
+    let json_op_codes: Value = serde_json::from_str(&op_codes_content).unwrap();
+
+    let unprefixed_op_codes: HashMap<String, Instruction> = get_instructions_from_json(&json_op_codes,"unprefixed");
+    let prefixed_op_codes: HashMap<String, Instruction> = get_instructions_from_json(&json_op_codes,"unprefixed");
+
+    println!("{:?}", unprefixed_op_codes["0x00"]);
+    println!("{:?}", prefixed_op_codes["0xFF"]);
 }
---
+++ src/op_codes_parser.rs
@@ -0,0 +1,78 @@
+pub mod op_codes_parser {
+    use std::collections::HashMap;
+    use std::fmt;
+    use std::fmt::Formatter;
+    use serde_json::Value;
+
+
+    #[derive(Debug)]
+    pub enum AdjustTypes {
+        POSITIVE,
+        NEGATIVE
+    }
+
+    #[derive(Debug)]
+    pub struct Operand {
+        immediate: bool,
+        name: String,
+        bytes: u8,
+        value: Option<u8>,
+        adjust: Option<AdjustTypes>
+    }
+
+    #[derive(Debug)]
+    pub struct Instruction {
+        opcode: u8,
+        immediate: bool,
+        operands: Vec<Operand>,
+        cycles: Vec<u8>,
+        bytes: u8,
+        mnemonic: &'static str,
+        comment: &'static str
+    }
+
+    impl fmt::Display for AdjustTypes {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "")
+        }
+    }
+
+    impl fmt::Display for Operand {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "{name} ha {bytes} bytes", name = self.name, bytes = self.bytes)
+        }
+    }
+
+
+    pub fn get_instructions_from_json(json_op_codes: &Value, category: &str) -> HashMap<String, Instruction> {
+        json_op_codes[category].as_object().unwrap().into_iter().map(|op_object| {
+            let op_code: String = op_object.0.as_str().to_owned();
+            let op_info = op_object.1.as_object().unwrap();
+
+            let collection_of_op: Vec<Operand> = op_info["operands"].as_array().unwrap().into_iter().map(|operand| {
+                let operand_object = operand.as_object().unwrap();
+                Operand {
+                    immediate: operand_object["immediate"].as_bool().expect("operand should be boolean"),
+                    name: operand_object["name"].as_str().expect("operand should be string").parse().unwrap(),
+                    bytes: op_info["bytes"].as_i64().expect("invalid number").to_le_bytes()[0],
+                    value: None,
+                    adjust: None
+                }
+            }).collect::<Vec<Operand>>();
+
+            let op_code_as_integer = u8::from_str_radix(op_code.trim_start_matches("0x"), 16).expect("invalid hex value");
+
+            let instruction = Instruction {
+                opcode: op_code_as_integer,
+                immediate: op_info["immediate"].as_bool().expect("invalid bool"),
+                operands: collection_of_op,
+                cycles: vec![],
+                bytes: op_info["bytes"].as_i64().expect("invalid number").to_le_bytes()[0],
+                mnemonic: "",
+                comment: ""
+            };
+            (op_code, instruction)
+        }).collect()
+    }
+
+}
