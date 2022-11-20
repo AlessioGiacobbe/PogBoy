cea5616360157f660a456f701c28c30ff03ab37e
Alessio Giacobbe <alessiogiacobbe@MacBook-Pro-di-Alessio.local>
2022-11-20 01:42:53 +0100

    Implemented cartridge header parsing

---
+++ src/cartridge.rs
@@ -0,0 +1,90 @@
+pub mod cartridge {
+    use std::fmt;
+    use std::fmt::Formatter;
+    use std::fs::File;
+    use std::io::Read;
+    use serde::{Serialize, Deserialize};
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct CartridgeInfo {
+        entry_point: [u16; 2],
+        nintendo_logo: [u16; 24],
+        title: [u8; 15],
+        //unused
+        cgb_flag: u8,
+        //licensee code ASCII, considered if old_license_code is 0x33
+        new_licensee_code: [u8; 2],
+        //supports sgb?
+        sgb_flag: u8,
+        //defines hardware on cartridge
+        cartridge_type: u8,
+        //rome size, given by 32 KiB × (1 << <value>)
+        rom_size: u8,
+        ram_size: u8,
+        //japan or not :)
+        destination_code: u8,
+        old_licensee_code: u8,
+        //rom version number, usually 0
+        mask_rom_version: u8,
+        //value to check header checksum against
+        header_checksum: u8,
+        //used only for pokemon
+        global_checksum: u16,
+    }
+
+    impl fmt::Display for CartridgeInfo {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "Cartridge : {{
+            entry_point: 0x{:04X} 0x{:04X}
+            cgb: {:?}
+            game_title: {:?}
+            sgb_flag: {}
+            cartridge_type: {}
+            rom_size: {}
+            ram_size: {}
+            dest_code: {}
+            header_checksum: 0x{:04X}
+            global_checksum: 0x{:04X}
+        }}",
+                   self.entry_point[0], self.entry_point[1],
+                   self.cgb_flag,
+                   self.game_title(),
+                   self.sgb_flag,
+                   self.cartridge_type,
+                   self.rom_size,
+                   self.ram_size,
+                   if self.destination_code == 0 {"Jap"} else {"Non-Jap"},
+                   self.header_checksum,
+                   self.global_checksum)
+        }
+    }
+
+    impl CartridgeInfo {
+        fn game_title(&self) -> &str {
+            match std::str::from_utf8(&self.title) {
+                Ok(value) => {
+                    value
+                }
+                Err(error) => {
+                    println!("Can't read rom name from header! :( {}", error);
+                    "BAD HEADER"
+                }
+            }
+
+        }
+    }
+
+    //TODO should return a real cartridge, not only Header
+    pub fn read_cartridge(file_name: &str) -> CartridgeInfo {
+        let mut rom = File::open(format!("./src/{}", file_name)).expect("rom not found");
+
+        let mut rom_bugger = Vec::new();
+        rom.read_to_end(&mut rom_bugger);
+
+        let header_start = usize::from_str_radix("0100", 16).expect("invalid hex");
+        let header_end = usize::from_str_radix("014F", 16).expect("invalid hex");
+        let rom_header: &[u8] = &rom_bugger[header_start..header_end+1];
+
+        bincode::deserialize(rom_header).unwrap()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 mod op_codes_parser;
+mod cartridge;
 
-use std::{fs};
+use std::{fmt, fs, usize};
 use std::collections::HashMap;
+use std::fmt::Formatter;
+use std::fs::File;
+use std::io::{Read};
 use serde_json::{Value};
+use crate::cartridge::cartridge::{CartridgeInfo, read_cartridge};
 use crate::op_codes_parser::op_codes_parser::Instruction;
 use crate::op_codes_parser::op_codes_parser::get_instructions_from_json;
 
+
+
+
 fn main() {
-    let op_codes_content = fs::read_to_string("./src/opcodes.json").expect("LogRocket: error reading file");
+    let op_codes_content = fs::read_to_string("./src/opcodes.json").expect("error reading file");
     let json_op_codes: Value = serde_json::from_str(&op_codes_content).unwrap();
 
     let unprefixed_op_codes: HashMap<String, Instruction> = get_instructions_from_json(&json_op_codes,"unprefixed");
     let prefixed_op_codes: HashMap<String, Instruction> = get_instructions_from_json(&json_op_codes,"unprefixed");
+    let cartridge_header = read_cartridge("snake.gb");  //TODO should be parameter
 
-    println!("{:?}", unprefixed_op_codes["0x00"]);
-    println!("{:?}", prefixed_op_codes["0xFF"]);
+    println!("{}", read_cartridge("snake2.gb"));
 }
