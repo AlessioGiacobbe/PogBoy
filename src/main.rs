5ebe38da6d1cddd6497170e49650b9c855d72954
Alessio Giacobbe <alessiogiacobbe@MacBook-Pro-di-Alessio.local>
2022-11-26 21:03:11 +0100

    Rom disassembler WIP

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@ mod cartridge;
 use std::{fmt, fs, usize};
 use std::collections::HashMap;
 use std::fmt::Formatter;
-use std::fs::File;
-use std::io::{Read};
+use std::fs::{File, read};
+use std::io::{Cursor, Read};
+use bincode::config::BigEndian;
 use serde_json::{Value};
 use crate::cartridge::cartridge::{CartridgeInfo, read_cartridge};
 use crate::op_codes_parser::op_codes_parser::{AdjustTypes, Instruction};
 use crate::op_codes_parser::op_codes_parser::get_instructions_from_json;
+use byteorder::{BigEndian as byteorderbigendian, ReadBytesExt};
+
+const INSTRUCTIONS_PREFIX: u8 = 203; //0xCB
 
 #[derive(Debug)]
 struct Decoder {
@@ -19,6 +23,32 @@ struct Decoder {
     address: u32,
     unprefixed_op_codes: HashMap<u8, Instruction>,
     prefixed_op_codes: HashMap<u8, Instruction>,
+}
+
+impl Decoder {
+
+    fn decode(&self, mut address: i32) {
+        let mut op_code = Self::read(&self, address, 1);
+        address = address + 1;
+        let instruction = {
+            if op_code[0] == INSTRUCTIONS_PREFIX {
+                op_code = Self::read(&self, address, 1);
+                address = address + 1;
+                self.prefixed_op_codes.get(&op_code[0])
+            }else{
+                self.unprefixed_op_codes.get(&op_code[0])
+            }
+        };
+    }
+
+    fn read(&self, address: i32, count: u8) -> &[u8] {
+        let end_address = address + i32::from(count);
+        if end_address >= 0 && end_address <= self.data.len() as i32 {
+            &self.data[address as usize..end_address as usize]
+        }else{
+            panic!("{} address out of bound!", self.data.len() as i32)
+        }
+    }
 }
 
 
@@ -30,13 +60,10 @@ fn main() {
     let prefixed_op_codes: HashMap<u8, Instruction> = get_instructions_from_json(&json_op_codes,"unprefixed");
     let cartridge_header = read_cartridge("snake.gb");  //TODO should be parameter
 
-    println!("{}", read_cartridge("snake.gb"));
-
-    println!("{:?}", unprefixed_op_codes[&0]);
-
     let mut rom = File::open(format!("./src/{}", "snake.gb")).expect("rom not found");
-
     let mut rom_buffer: Vec<u8> = Vec::new();
+    rom.read_to_end(&mut rom_buffer);
+
 
     let decoder: Decoder = Decoder {
         data: rom_buffer,
@@ -45,5 +72,7 @@ fn main() {
         prefixed_op_codes
     };
 
-    //println!("{:?}", decoder);
+    println!("{:?}", decoder.read(64, 2));
+    println!("{:?}", decoder.decode(64));
+    println!("{:?}", decoder.decode(63));
 }
