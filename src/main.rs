8ee287aef0ab6ee06e3f107c144110233e3d768a
Alessio Giacobbe <giacobbealessio@gmail.com>
2022-12-04 01:58:17 +0100

    main cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,15 @@
-extern crate core;
-
 mod op_codes_parser;
 mod cartridge;
 mod decoder;
 mod cpu;
 
-use std::{fmt, fs};
-use std::collections::HashMap;
-use std::fmt::Formatter;
-use std::fs::{File};
-use std::io::{Read};
-use serde_json::{Value};
-use crate::cartridge::cartridge::{Cartridge, CartridgeInfo, read_cartridge};
-use crate::decoder::decoder::{Decoder};
-use crate::op_codes_parser::op_codes_parser::{Instruction};
-use crate::op_codes_parser::op_codes_parser::get_instructions_from_json;
+use crate::cartridge::cartridge::{Cartridge, read_cartridge};
 use crate::cpu::CPU::CPU;
+use crate::decoder::decoder::Decoder;
+
 
 fn main() {
     let Cartridge: Cartridge = read_cartridge("snake.gb");
     let Decoder: Decoder = Decoder::new(Cartridge);
-    let cpu: CPU = CPU::new(Decoder);
+    let CPU: CPU = CPU::new(Decoder);
 }
