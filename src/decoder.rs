18125ecedbe74e53f9e0ec9bb7590c3083127f1d
Alessio Giacobbe <alessiogiacobbe@MacBook-Pro-di-Alessio.local>
2022-12-04 01:41:52 +0100

    CPU struct WIP + reafactored Decoder

--- src/decoder.rs
+++ src/decoder.rs
@@ -1,19 +1,44 @@
-pub mod disassembler {
+pub mod decoder {
     use std::collections::HashMap;
+    use std::fs;
+    use std::fs::File;
+    use std::io::Read;
     use byteorder::{LittleEndian as byteorderLittleEndian, ReadBytesExt};
-    use crate::op_codes_parser::op_codes_parser::{Instruction, Operand, OperandValue};
+    use serde_json::Value;
+    use crate::cartridge::cartridge::read_cartridge;
+    use crate::op_codes_parser::op_codes_parser::{get_instructions_from_json, Instruction, Operand, OperandValue};
 
     const INSTRUCTIONS_PREFIX: u8 = 203; //0xCB
 
     #[derive(Debug)]
-    pub struct Disassembler {
+    pub struct Decoder {
         pub(crate) data: Vec<u8>,
         pub(crate) address: u32,
         pub(crate) unprefixed_op_codes: HashMap<u8, Instruction>,
         pub(crate) prefixed_op_codes: HashMap<u8, Instruction>,
     }
 
-    impl Disassembler {
+    impl Decoder {
+
+        pub(crate) fn new() -> Decoder {
+            let op_codes_content = fs::read_to_string("./src/opcodes.json").expect("error reading file");
+            let json_op_codes: Value = serde_json::from_str(&op_codes_content).unwrap();
+
+            let unprefixed_op_codes: HashMap<u8, Instruction> = get_instructions_from_json(&json_op_codes,"unprefixed");
+            let prefixed_op_codes: HashMap<u8, Instruction> = get_instructions_from_json(&json_op_codes,"unprefixed");
+
+            let mut rom = File::open(format!("./src/{}", "snake.gb")).expect("rom not found");
+            let mut rom_buffer: Vec<u8> = Vec::new();
+            rom.read_to_end(&mut rom_buffer).expect("can't read ROM");
+
+
+            Decoder {
+                data: rom_buffer,
+                address: 0,
+                unprefixed_op_codes,
+                prefixed_op_codes
+            }
+        }
 
         pub(crate) fn read(&self, address: i32, count: u8) -> &[u8] {
             let end_address = address + i32::from(count);
--- src/tests.rs
+++ src/tests.rs
@@ -10,3 +10,38 @@ fn test_little_big_endian(){
         println!("{:#04x}", to_be_byte);
     }
 }
+
+/*
+TODO create real tests
+            registers.set_item("F", 8);
+            println!("{:#01x}", registers.get_item("AF"));
+
+            registers.set_item("A", 8);
+            println!("{:#01x}", registers.get_item("AF"));
+
+            registers.set_item("AF", 44975);
+            println!("{:#01x}", registers.get_item("AF"));
+
+
+            println!("{:#01x}", registers.get_item("B"));
+            println!("{:#01x}", registers.get_item("D"));
+            println!("{:#01x}", registers.get_item("c"));
+            println!("{:#01x}", registers.get_item("BC"));
+            println!("{:#01x}", registers.get_item("AF"));
+
+            registers.set_item("AF", 0);
+            registers.set_item("c", 1);
+            registers.set_item("h", 1);
+            registers.set_item("n", 1);
+            registers.set_item("z", 1);
+            registers.set_item("AF", 65535);
+            registers.set_item("c", 0);
+            registers.set_item("h", 0);
+            registers.set_item("n", 0);
+            registers.set_item("z", 0);
+
+
+    println!("{:?}", decoder.read(359, 2));
+    println!("{:?}", decoder.decode(359).1);
+    println!("{}", decoder.decode(359).1);
+ */
