d776fd890dd3b4a98c25b2a6a3e03f3ce724b6df
Alessio Giacobbe <giacobbealessio@gmail.com>
2023-02-04 13:43:53 +0100

    input handling WIP

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ mod tests;
 
 use std::borrow::Borrow;
 use std::sync::mpsc;
-use std::sync::mpsc::RecvError;
+use std::sync::mpsc::{Receiver, RecvError, Sender, SyncSender};
 use std::thread;
-use piston_window::{clear, Context, Event, Glyphs, Input, math, PistonWindow, rectangle, text, TextureSettings, WindowSettings};
+use piston_window::{Button, ButtonState, clear, Context, Event, Glyphs, Input, Key, math, PistonWindow, rectangle, text, TextureSettings, WindowSettings};
 use piston_window::glyph_cache::rusttype::GlyphCache;
 use piston_window::types::{Color, Matrix2d};
 use crate::cartridge::cartridge::{Cartridge, read_cartridge};
@@ -24,27 +24,10 @@ use crate::ppu::ppu::PPU;
 
 fn main() {
 
-    let (cpu_sender, window_receiver) = mpsc::sync_channel(1);
-    let (window_sender, cpu_receiver) = mpsc::channel();
+    let (cpu_sender, window_receiver) : (SyncSender<u32>, Receiver<u32>) = mpsc::sync_channel(1);
+    let (window_sender, cpu_receiver) : (Sender<bool>, Receiver<bool>) = mpsc::channel();
 
-    let cpu_thread = thread::spawn(move|| {
-        let cartridge: Cartridge = read_cartridge("image.gb");
-        let mut ppu: PPU = PPU::new();
-        let mut mmu: MMU = MMU::new(Some(cartridge), &mut ppu);
-        let mut cpu: CPU = CPU::new(mmu);
-
-        loop {
-            let clock = cpu.step();
-            cpu.MMU.PPU.step(clock);
-            let _ = cpu_sender.send(clock).expect("errore");
-
-            //TODO receive real input from window key press
-            let received = cpu_receiver.try_recv();
-            if received.is_ok() {
-                break
-            }
-        }
-    });
+    let cpu_thread = thread::spawn(move|| run_cpu(cpu_sender, cpu_receiver));
 
     let mut window: PistonWindow = WindowSettings::new("Pog!", [160, 144]).exit_on_esc(true).build().unwrap();
 
@@ -54,16 +37,28 @@ fn main() {
         match event {
             Event::Input(input, _) => {
                 match input {
-                    Input::Button(_) => {}
-                    Input::Move(_) => {}
-                    Input::Text(_) => {}
-                    Input::Resize(_) => {}
-                    Input::Focus(_) => {}
-                    Input::Cursor(_) => {}
-                    Input::FileDrag(_) => {}
+                    Input::Button(ButtonArgs) => {
+                        match ButtonArgs.state {
+                            ButtonState::Press => {
+                                match ButtonArgs.button {
+                                    Button::Keyboard(Key) => {
+                                        match Key {
+                                            Key::Escape => {
+                                                window_sender.send(true).unwrap();
+                                            }
+                                            _ => {}
+                                        }
+                                    }
+                                    _ => {}
+                                }
+                            }
+                            ButtonState::Release => {}
+                        }
+                    }
                     Input::Close(_) => {
                         window_sender.send(true).unwrap();
                     }
+                    _ => {}
                 }
             }
             Event::Loop(_) => {
@@ -91,4 +86,23 @@ fn main() {
 
     let _ = cpu_thread.join();
 
+}
+
+fn run_cpu(cpu_sender: SyncSender<u32>, cpu_receiver: Receiver<bool>) {
+    let cartridge: Cartridge = read_cartridge("image.gb");
+    let mut ppu: PPU = PPU::new();
+    let mut mmu: MMU = MMU::new(Some(cartridge), &mut ppu);
+    let mut cpu: CPU = CPU::new(mmu);
+
+    loop {
+        let clock = cpu.step();
+        cpu.MMU.PPU.step(clock);
+        let _ = cpu_sender.send(clock).expect("error sending to window");
+
+        //TODO receive real input from window key press
+        let received = cpu_receiver.try_recv();
+        if received.is_ok() {
+            break
+        }
+    }
 }
